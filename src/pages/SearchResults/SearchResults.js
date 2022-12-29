119c839bbf636f0a4d5a45b618d84065761e9dc2
banksjt21 <113642612+banksjt21@users.noreply.github.com>
2022-12-29 02:31:45 -0500

    'Add to Collection' button on Search page is working

--- src/pages/SearchResults/SearchResults.js
+++ src/pages/SearchResults/SearchResults.js
@@ -12,7 +12,8 @@
 //  =======================================================================  */
 import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import Books from '../Books/Books';
+import { useNavigate } from 'react-router-dom';
+import { addBook } from '../../utilities/books-api';
 
 
 
@@ -22,12 +23,51 @@ import Books from '../Books/Books';
 //  =======================================================================  */
 export default function SearchResults() {
 
+    const navigate = useNavigate();
     const location = useLocation();
 
 
     const [googleBooks, setGoogleBooks] = useState(location.state.searchResults.items);
 
     console.log(googleBooks);
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        console.log(event);
+
+        const selectedID = event.target.googleBookID.value;
+        console.log(selectedID);
+
+        const selectedBook = googleBooks.find(theBook => theBook.id === selectedID);
+        console.log(selectedBook);
+
+        // Some search results do not have the imageLinks property ...
+        if (selectedBook.volumeInfo.imageLinks === undefined) {
+            console.log(selectedBook.imageLinks)
+            Object.defineProperty(selectedBook.volumeInfo, 'imageLinks', {
+                value: {thumbnail: ''},
+                writable: true
+            });
+        }
+
+        //  Prepare data to be sent to server
+        const bookData = {
+            title: selectedBook.volumeInfo.title,
+            author: selectedBook.volumeInfo.authors.toString() || ["No Author"],
+            description: selectedBook.volumeInfo.description || "No Description",
+            year: selectedBook.volumeInfo.publishedDate || "No Date",
+            category: selectedBook.volumeInfo.categories.toString() || ["No Category"],
+            image: selectedBook.volumeInfo.imageLinks.thumbnail
+        }
+        console.log(bookData);
+
+        const book = await addBook(bookData);
+        navigate('/books');
+        console.log(book);
+    }
+
+
 
     const loaded = () => {
         return (
@@ -37,23 +77,23 @@ export default function SearchResults() {
                     googleBooks.map((book, i) => {
 
                         let googleBookID = book.id;
-                        console.log(googleBookID);
+                        // console.log(googleBookID);
 
                         book = book.volumeInfo;
 
                         //  Handle results that are missing information
                         let bookCover;
-                        if(book.imageLinks) {
+                        if (book.imageLinks) {
                             bookCover = book.imageLinks.thumbnail;
                         } else {
                             bookCover = '/book-cover-placeholder.png'
                         }
-                        
-                        book.categories           = book.categories || ["No Category"];
-                        book.publishedDate        = book.publishedDate || "No Year";
-                        book.description          = book.description || "No Description";
-                        book.publisher            = book.publisher || "No Publisher";
-                        book.authors              = book.authors || ["No Author"];
+
+                        book.categories = book.categories || ["No Category"];
+                        book.publishedDate = book.publishedDate || "No Year";
+                        book.description = book.description || "No Description";
+                        book.publisher = book.publisher || "No Publisher";
+                        book.authors = book.authors || ["No Author"];
 
 
 
@@ -82,6 +122,13 @@ export default function SearchResults() {
                                         })
                                     }
                                 </ul>
+
+
+
+                                <form autoComplete="off" onSubmit={handleSubmit}>
+                                    <input type="hidden" name="googleBookID" value={googleBookID} />
+                                    <input type="submit" value="Add to Collection" />
+                                </form>
                             </div>
                         )
                     })
